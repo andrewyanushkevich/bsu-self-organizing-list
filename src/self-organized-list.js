a36a4cfa399531c69331672000b39e26ac342514
Andrew <andy.yanushkevich@gmail.com>
2018-02-19 10:31:07 +0300

    Adding list

--- src/self-organized-list.js
+++ src/self-organized-list.js
@@ -5,35 +5,87 @@ class Node {
         this.prev = null;
     }
 }
-
-class SelfOrganizedList {
-    constructor() {
+class SelfOrganizedList{
+    constructor(){
         this.head = null;
         this.tail = null;
+        this.length = 0;
     }
-
-    insert(data) {
-
+     insert(data){
+        var node = new Node(data);
+        if(this.head == null){
+            this.head = node;
+            this.tail = this.head;
+            this.head.next= null;
+            this.tail.prev = null;
+            this.length++;
+        }
+        else{
+            this.tail.next = node;
+            node.prev = this.tail;
+            this.tail = node;
+            this.tail.next = null;
+            this.length++;
+        }
     }
-
-    size() {
-
+    size(){
+        return this.length;
     }
-
-    at(index) {
-
+    at(index){
+        if(this.head == null || index > this.length){
+            return null;
+        }
+        var i = 0;
+        var temp = this.head;
+        while(i !== index){
+            temp = temp.next;
+            i++;
+        }
+        return temp.data;
     }
-
     findNode(data) {
-
+        var temp = this.head;
+        while(temp != null){
+            if(temp.data == data){
+                return temp;
+            }
+            temp = temp.next;
+        }
+        return null;
     }
 
     toArray() {
-
+        var array = [];
+        var temp = this.head;
+        while(temp != null){
+            array.push(temp.data);
+            temp = temp.next;
+        }
+        return array;
     }
 
     removeAt(index) {
-
+        var i = 1;
+        var temp = this.head;
+        if(index == 1){
+            var head = this.head;
+            this.head = this.head.next;
+            return;
+        }
+        while(i != index){
+            temp = temp.next;
+            i++;
+        }
+        if(temp == tail){
+            var tail = this.tail;
+            this.tail = this.tail.prev;
+            return;
+        }
+        var prev = temp.prev;
+        var next = temp.next;
+        prev.next = next;
+        next.prev = prev;
+        this.length--;
     }
 
     moveToFront(node) {
@@ -43,7 +95,6 @@ class SelfOrganizedList {
     reorganize(data) {
 
     }
-
 }
 
 module.exports = {
